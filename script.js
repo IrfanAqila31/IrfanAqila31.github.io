82867e4b875c35938938c93337639fcf02a3ace2
IrfanAqila31 <121709140+IrfanAqila31@users.noreply.github.com>
2025-02-10 11:34:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let nama;
+
+while (true) {
+  // Loop akan berjalan terus sampai kondisi terpenuhi
+  nama = prompt("Siapa Pacar Aku");
+
+  if (nama == "Patricia Servana") {
+    alert("Beneeeer");
+    break; // Keluar dari loop jika jawaban benar
+  } else {
+    alert("Salah Kocak");
+  }
+}
